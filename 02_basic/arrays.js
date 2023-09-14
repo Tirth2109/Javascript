dfc4d957c9f3f86fabbf40439a2419cbed6eb21d
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-14 19:26:47 +0530

    updatefile 5

---
+++ 02_basic/arrays.js
@@ -0,0 +1,24 @@
+const myarry = [0, 1, 12, 43, 54]
+
+console.log(myarry[4]);
+
+//array method
+
+myarry.push(8)
+myarry.push(102)
+myarry.pop()   // its remove the last value of the array
+myarry.pop()
+
+
+myarry.unshift(10000)
+myarry.shift()
+
+
+console.log(myarry.includes(1));// its show the number is in the array or not
+console.log(myarry.indexOf(12));// its show the number is at which position
+
+const newarray = myarry.join()
+console.log(myarry);
+console.log(newarray);
+
+

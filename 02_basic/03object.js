89eab99207d010a703061a9d9025201c321eedff
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-17 21:34:13 +0530

    updatefile 5

--- 02_basic/03object.js
+++ 02_basic/03object.js
@@ -1,5 +1,33 @@
+// singleton
+
+//object literals
+
+const mysyn = Symbol("key1")
+
+const jsuser = {
+    name: "tirth",
+    age: 20,
+    socirty: "kadi",
+    email: "tirth@MessageChannel.com",
+    [mysyn]: "mykey1"
+}
+
+console.log(jsuser.age);
+console.log(jsuser["name"]);
+console.log(jsuser[mysyn]);
+
+jsuser.email = "tirth@megha.com"
+//     Object.freeze(jsuser)
+jsuser.email = "jrifrreh"
+console.log(jsuser);
 
 
+jsuser.greeting =function() {
+    console.log("HEllo js my faverate");
+}
+jsuser.greetingtwo =function() {
+    console.log(`HEllo js my faverate, ${this.socirty}`);
+}
 
-
-console.log("tirthmpatel");
+console.log(jsuser.greeting());
+console.log(jsuser.greetingtwo());
---
+++ 02_basic/04object.js
@@ -0,0 +1,16 @@
+const tinderuser = new Object()
+
+tinderuser.id ="123ddw"
+tinderuser.name="tirth"
+tinderuser.isLoggedIn = false
+const regularuser = {
+ emil: "saome@gmail.com",
+ fullname:{
+    userfullname:{
+        fristname:"tirth",
+        lastname:"patel"
+    }
+ }   
+}
+console.log(regularuser);
+//console.log(tinderuser);

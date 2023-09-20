64410272a0b92d741b31f8be13051c096aa95311
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-20 12:27:07 +0530

    updatefile 5

--- 02_basic/04object.js
+++ 02_basic/04object.js
@@ -21,3 +21,33 @@ const obj2 = {3:"a", 4:"t"}
 // const obj3 = {obj1, obj2}     not a good syntex not a perfect method to write
 const obj3 = Object.assign(obj1, obj2)
 console.log(obj3);
+
+const users = [
+    {
+    id:1,
+    email:"H@gamil.com"
+},
+{
+    id:1,
+    email:"H@gamil.com"
+},
+{
+    id:1,
+    email:"H@gamil.com"
+},
+{
+    id:1,
+    email:"H@gamil.com"
+}
+] 
+
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+console.log(tinderuser.hasOwnProperty('isLogged'));
+
+
+

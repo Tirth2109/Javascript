d9772587063a300017583647114938732c6fb08d
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-24 17:55:29 +0530

    updatefile 5

---
+++ 03_basic/01_function.js
@@ -0,0 +1,38 @@
+
+function myname(){
+console.log("t");
+}
+myname()
+
+function addtownumber(number1,number2){
+console.log(number1+ number2);
+ }
+const result = addtownumber(3,5)
+
+
+
+
+
+
+function addtownumber1(number1,number2){
+   // console.log(number1+ number2);
+    let resultfinal = number1 + number2
+    return resultfinal
+}
+const resultfinal = addtownumber1(3,9)
+console.log("Result:", resultfinal);
+
+
+
+
+
+
+function loginusermess(username){
+    if(username === undefined){
+        console.log("please enterthe username");
+        return
+    }
+
+    return`${username}you just logged in`
+}
+console.log(loginusermess("tirth wellcome "));

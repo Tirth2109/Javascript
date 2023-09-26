1935982cfcff64efbca60e0d4b0e9ae46930e731
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-26 18:27:41 +0530

    updatefile 5

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -7,8 +7,29 @@ if (true){
 
 }
 
+
     
 
 console.log(a);
 //console.log(c);
 //console.log(b);
+
+function one(){
+    const username = "tirth"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+    two()
+}
+one()
+
+if (true){
+    const username ="tirth"
+    if(username==="tirth"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+}

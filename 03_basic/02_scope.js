5c341c43239b67cc356609aabfa779db3a006b76
Tirth2109 <tirthmpatel21@gmail.com>
2023-09-26 18:39:08 +0530

    updatefile 5

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -33,3 +33,17 @@ if (true){
         console.log(username + website);
     }
 }
+
+
+//*********************intresting*************** */
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+
+}
+
+console.log(addtwo(5));
+const addtwo = function(num){
+    return num + 2
+}

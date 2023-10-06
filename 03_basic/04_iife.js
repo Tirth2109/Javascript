f55f1d7e311cc00c9f96a4360d1ee8a737fafc7f
Tirth2109 <tirthmpatel21@gmail.com>
2023-10-06 21:34:25 +0530

    updatefile 5

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -16,5 +16,5 @@
 
 ((name) => {
     //  also called symbol iife  of all
-    console.log(`Db connected of are and  two${name}`);
+    console.log(`Db connected of are and   tutbjfjfhref two${name}`);
 })('tirth')
